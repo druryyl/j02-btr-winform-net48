9c0f5d5342c134426a4d009d1e48e76906e1be4c
Jude <drury_yl@yahoo.com>
2023-06-29 14:24:22 +0700

    Create New Clean WinForm

--- btr.winform48/Helper/JSendResponseReader.cs
+++ btr.winform48/Helper/JSendResponseReader.cs
@@ -1,5 +1,4 @@
-﻿using Dawn;
-using Newtonsoft.Json;
+﻿using System.Text.Json;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -15,19 +14,16 @@ namespace btr.winform48.Helper
     {
         public static T Read<T>(RestResponse<JSend<T>> response)
         {
-            Guard.Argument(() => response).NotNull();
             if (response.Data is null)
                 ReadAndThrowError(response);
             return response.Data.Data;
         }
         private static void ReadAndThrowError(RestResponseBase response)
         {
-            Guard.Argument(() => response).NotNull();
-
             if (response.Content is null)
                 throw new InvalidOperationException($"Error Remote: ({(int)response.StatusCode}) {response.ErrorException.Message}");
 
-            var resultFailed = JsonConvert.DeserializeObject<JSend<string>>(response.Content);
+            var resultFailed = JsonSerializer.Deserialize<JSend<string>>(response.Content);
             if (resultFailed != null)
                 throw new ArgumentException(resultFailed.Data);
             else
